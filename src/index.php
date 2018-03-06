ddfc2e7fe57c95ca8df18ba329740502e905d38f
sesame437 <35056873+sesame437@users.noreply.github.com>
2018-03-06 21:54:30 +0800

    demo

--- src/index.php
+++ src/index.php
@@ -18,7 +18,7 @@
                 <h1>QianKai's master builder</h1>
                 <h2>请各位高抬贵手</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
-                <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+                <p>The container is running PHP version -demo <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
