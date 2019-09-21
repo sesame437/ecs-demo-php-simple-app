c57715ddcd1026b33a061aafbc72ba2a38b48e4c
sesame437 <35056873+sesame437@users.noreply.github.com>
2019-09-21 12:07:11 +0800

    demo

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>QianKai's master builder</h1>
-                <h2>各位大哥高抬贵手！</h2>
+                <h1>Demo</h1>
+                <h2>Demo</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version -demo <?php echo phpversion(); ?>.</p>
                 <?php
